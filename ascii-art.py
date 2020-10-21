5c2de09ae9aaec6c2665492d3da0e68eeed92eed
AmazonianPanda <howyoudoing1618@gmail.com>
2020-10-21 22:30:45 +0530

    Importing libraries

--- ascii-art.py
+++ ascii-art.py
@@ -1,6 +1,16 @@
+# Import Libraries
+# from PIL import Image
+import PIL
+
+
+
 # Constants
 ASCII_IMAGE_WIDTH = 150
 PIXEL_ARRAY = ["#","@","O","=","+","o","^","*","!","."]
 LENGTH = len(PIXEL_ARRAY)
 RANGE = 255/LENGTH
 
+def get_image():
+    location = input("Enter the Absolute or Relative location")
+    # img = 
+    pass
