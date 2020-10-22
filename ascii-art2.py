12791221863164ea2732bda31d203bf2e8e67520
AmazonianPanda <howyoudoing1618@gmail.com>
2020-10-22 21:27:42 +0530

    second version added

---
+++ ascii-art2.py
@@ -0,0 +1,25 @@
+# Import Libraries
+import PIL.Image
+
+
+# Constants
+ASCII_IMAGE_WIDTH = 150
+PIXEL_ARRAY = ["#","@","O","=","+","o","^","*","!","."]
+LENGTH = len(PIXEL_ARRAY)
+RANGE = 255/LENGTH
+
+
+def get_img(image):
+
+    pass
+
+def pixels_to_ascii_string(image):
+    pass
+
+def set_line_break(stringOfChar):
+    pass
+
+def main():
+    loc = input("Enter the Location of image")
+
+    pass
