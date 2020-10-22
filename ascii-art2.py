24f5239e92dbc151dd2fd569929cf74e5d30df2d
AmazonianPanda <howyoudoing1618@gmail.com>
2020-10-22 22:42:42 +0530

    ascii-art3.txt also filled

--- ascii-art2.py
+++ ascii-art2.py
@@ -1,29 +1,53 @@
 # Import Libraries
-from PIL import Image
-
+from PIL import Image, ImageOps
+import numpy as np
 
 # Constants
-ASCII_IMAGE_WIDTH = 150
+ASCII_IMAGE_WIDTH = 100
 PIXEL_ARRAY = ["#","@","O","=","+","o","^","*","!","."]
 LENGTH = len(PIXEL_ARRAY)
 RANGE = int(255/(LENGTH - 1))
 
 
 def get_img(image):
+    """This Function returns the grayscale resized image of the imput image given as parameter
+
+    Args:
+        image (ImageObj): PIL.Image type of image
+
+    Returns:
+        ImageObj: grayscale value
+    """
     actual_width, actual_height = image.size
     new_height = int(actual_height*ASCII_IMAGE_WIDTH/actual_width)
     resized_image = image.resize((ASCII_IMAGE_WIDTH,new_height))
     return resized_image.convert('L')
 
+
 def pixels_to_ascii_string(image):
+    """This method receives the image object and get the individual
+    pixel value of each pixel and get a string curresponding to this 
+    number from the PIXEL_ARRAY and adds it to the string.
+        after every ASCII_IMAGE_WIDTH it adds a new Line Character to
+    the String and finally returns this String
+
+    Args:
+        image (ImageObj): Gets the Image(Should be a 2-D array, so not color images)
+
+    Returns:
+        String: The string which is needed to be printedto the file
+    """
     pixels = image.getdata()
-    charecters = "".join( [PIXEL_ARRAY[pixel//(RANGE)] for pixel in pixels] )
-    return charecters
+    width, height = image.size
+    imageArray = np.asarray(image)
+    # print(imageArray)
+    string = ""
+    for i in range(height):
+        for j in range(width):
+            string += PIXEL_ARRAY[imageArray[i,j]//RANGE]
+        string += "\n"
+    return string
 
-
-def set_line_break(listOfChar):
-    new_data = "\n".join([listOfChar[index:(index+ASCII_IMAGE_WIDTH)] for index in range(0, len(listOfChar), ASCII_IMAGE_WIDTH)])
-    return new_data
 
 def main():
     loc = input("Enter the Location of image")
@@ -32,10 +56,12 @@ def main():
     except:
         print("{} is not a valid image address".format(loc))
 
-    new_image = pixels_to_ascii_string(get_img(image))
-    toPrint = set_line_break(pixels_to_ascii_string(get_img(image)))
+    toPrint = pixels_to_ascii_string(get_img(image))
 
-    with open("ascii-art2.txt", "w") as file:
+    with open("ascii-art3.txt", "w") as file:
         file.write(toPrint)
 
-main()
+
+
+if __name__ == "__main__":
+    main()
