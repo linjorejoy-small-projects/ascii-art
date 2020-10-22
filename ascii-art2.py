86d1edcb4f1499e266e53c680787d3e299b3ddaf
AmazonianPanda <howyoudoing1618@gmail.com>
2020-10-22 22:02:50 +0530

    second version completed

--- ascii-art2.py
+++ ascii-art2.py
@@ -1,25 +1,41 @@
 # Import Libraries
-import PIL.Image
+from PIL import Image
 
 
 # Constants
 ASCII_IMAGE_WIDTH = 150
 PIXEL_ARRAY = ["#","@","O","=","+","o","^","*","!","."]
 LENGTH = len(PIXEL_ARRAY)
-RANGE = 255/LENGTH
+RANGE = int(255/(LENGTH - 1))
 
 
 def get_img(image):
-
-    pass
+    actual_width, actual_height = image.size
+    new_height = int(actual_height*ASCII_IMAGE_WIDTH/actual_width)
+    resized_image = image.resize((ASCII_IMAGE_WIDTH,new_height))
+    return resized_image.convert('L')
 
 def pixels_to_ascii_string(image):
-    pass
+    pixels = image.getdata()
+    charecters = "".join( [PIXEL_ARRAY[pixel//(RANGE)] for pixel in pixels] )
+    return charecters
 
-def set_line_break(stringOfChar):
-    pass
+
+def set_line_break(listOfChar):
+    new_data = "\n".join([listOfChar[index:(index+ASCII_IMAGE_WIDTH)] for index in range(0, len(listOfChar), ASCII_IMAGE_WIDTH)])
+    return new_data
 
 def main():
     loc = input("Enter the Location of image")
+    try:
+        image = Image.open(loc)
+    except:
+        print("{} is not a valid image address".format(loc))
 
-    pass
+    new_image = pixels_to_ascii_string(get_img(image))
+    toPrint = set_line_break(pixels_to_ascii_string(get_img(image)))
+
+    with open("ascii-art2.txt", "w") as file:
+        file.write(toPrint)
+
+main()
